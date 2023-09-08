524a1c2c8275991e2ce37d48eb34759611ec3c96
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-09-08 15:27:09 +0200

    Initialize example-remove

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+// =============================================================================
+// Template-Project | Main
+// =============================================================================
+
+#include <Arduino.h>
+#include "hardware.h"
+#include "EdgeDetection.h"
+#include "configurations.h"
+#include "state.h"
+
+void refreshData();
+
+//------------------------------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  for (;;)
+  {
+    refreshData();
+
+    State::stateDriver();
+  }
+}
+
+//------------------------------------------------------------------------------
+
+void refreshData()
+{
+
+  Hardware::updateHardware();
+  EdgeDetection::updateEdges();
+}
