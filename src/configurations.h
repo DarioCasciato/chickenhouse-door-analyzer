3cb0d693aa150d6dd309bcd9824de5c35678a2fc
Dario Casciato <dario.casciato@dormakaba.com>
2023-09-15 11:31:43 +0200

    fic

--- src/configurations.h
+++ src/configurations.h
@@ -11,6 +11,6 @@
 
 inline const char* api_datetime = "http://worldtimeapi.org/api/timezone/Europe/Zurich";
 inline const char* api_suntime = "http://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=";
-inline const char* api_key = "512b5f06f3605c348cc9ef55e4618094";
+inline const char* api_key = "OPENWEATHERMAP_API_KEY";
 
 #endif // _TEMPLATE_PROJECT_CONFIGURATIONS_
