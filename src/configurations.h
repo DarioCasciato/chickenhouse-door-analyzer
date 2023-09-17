1059f17825694146c2fec135faba913d1283709d
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-09-17 21:24:33 +0200

    put ssid and password into api_key.h

--- src/configurations.h
+++ src/configurations.h
@@ -6,9 +6,6 @@
 #define _TEMPLATE_PROJECT_CONFIGURATIONS_
 
 
-
-
-
 inline const char* api_datetime = "http://worldtimeapi.org/api/timezone/Europe/Zurich";
 inline const char* api_openweather = "http://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=";
 inline const char* request_open = "http://maker.ifttt.com/trigger/DOOR_OPEN_EVENT/with/key/";
